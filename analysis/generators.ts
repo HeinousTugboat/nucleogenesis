a4021f8fff9c66f4f03ed5ac62a11ffc5960f6b7
HeinousTugboat <admin@heinousindustries.com>
2017-09-11 22:26:45 -0400

    analysis: add generator class and interfaces, builds generator list

--- analysis/generators.ts
+++ analysis/generators.ts
@@ -1,36 +1,47 @@
 import { Upgrade } from './upgrades';
-export interface Generator {
+
+export interface GenJSON {
     name: string;
     price: number;
     power: number;
     deps: string[];
-    num?: number;
-    basePower: number;
+    upgrades: string[];
     type: 'generator';
 }
 export interface IGen {
-    [key: string]: Generator
+    [key: string]: GenJSON
 }
 
 export function isGen(g: Generator | Upgrade): g is Generator {
     return (<Generator>g).type === 'generator';
 }
 
-export class Generator implements Generator {
+export class Generator {
     static list: Map<string, Generator> = new Map;
     public type: 'generator';
+    public name: string = 'Unnamed Generator';
     public price: number = 0;
     public power: number = 0;
     public deps: string[] = [];
-    public num?: number = 1;
+    public upgrades: string[] = [];
+    public num: number = 0;
     public basePower: number = 0;
-    constructor(public name: string) {
+    constructor(json: GenJSON) {
+        ({
+            price: this.price,
+            name: this.name,
+            power: this.power,
+            deps: [...this.deps],
+            upgrades: [...this.upgrades]
+        } = json);
+
         Generator.list.set(this.name, this);
     }
 
 }
 
+const gens: IGen = require('../build/data/generators.json');
 
-let g1 = new Generator('1');
-console.log(g1);
-console.log(Generator.list);
+for (const gen in gens) {
+    new Generator(gens[gen]);
+}
